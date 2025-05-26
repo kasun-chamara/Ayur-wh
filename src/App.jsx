bd5b08e3810ed40cd297823ad396d9c7d3581b07
kasun-chamara <kasunchamarawb@gmail.com>
2025-05-26 22:28:51 +0530

    Update 3.2

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import './App.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Header from './components/header/Header';
+import Header from './components/Header/Header';
 import Hero from './components/hero/Hero';
 import About from './components/about/About';
 import Call from './components/call/Call';
